a3543962d616f1fa04f79f01fa86a5a110075128
westbrook <westbrook.johnson@gmail.com>
2012-12-09 23:34:18 -0500

    Categories
    Load and print categories to prepare filtering for displayed results.
    
    Cache the api key for simpler URL setting and updating for possible
    future implementations.

--- js/etsy.js
+++ js/etsy.js
@@ -1,20 +1,26 @@
 window.app = {
 	collection: {},
 	model: {},
-	view: {}
+	view: {},
+	api_key: 'CMCCX1OZDKGQ4M1LA2GLLSJL'
 };
 app.model.Listing = Backbone.Model.extend({
 	initialize: function() {
 		_.bindAll(this);
 	},
 	url: function() {
-		return 	'http://openapi.etsy.com/v2/listings/'+this.id+'.js?api_key=CMCCX1OZDKGQ4M1LA2GLLSJL';	
+		return 	'http://openapi.etsy.com/v2/listings/'+this.id+'.js?api_key='+app.api_key+'';	
 	},
 	scope: function() {
 		var newAttrs = this.get('results')[0];
 		this.clear();
 		this.set(newAttrs);
 		this.trigger('scoped');
+	}
+});
+app.model.Category = Backbone.Model.extend({
+	url: function() {
+		return 'http://openapi.etsy.com/v2/taxonomy/categories/'+this.tag+'?api_key='+app.api_key;
 	}
 });
 app.collection.EtsySearch = Backbone.Collection.extend({
@@ -29,13 +35,18 @@ app.collection.EtsySearch = Backbone.Collection.extend({
 app.collection.Listings = app.collection.EtsySearch.extend({
 	model: app.model.Listing,
 	url: function() {
-		return 'http://openapi.etsy.com/v2/listings/active.js?api_key=CMCCX1OZDKGQ4M1LA2GLLSJL&limit='+this.limit+'&keywords='+this.searchTerm;	
+		return 'http://openapi.etsy.com/v2/listings/active.js?api_key='+app.api_key+'&limit='+this.limit+'&keywords='+this.searchTerm;	
 	},
 	limit: 10
 });
 app.collection.ProductImages = app.collection.EtsySearch.extend({
 	url: function() {
-		return 'http://openapi.etsy.com/v2/listings/'+this.id+'/images.js?api_key=CMCCX1OZDKGQ4M1LA2GLLSJL';	
+		return 'http://openapi.etsy.com/v2/listings/'+this.id+'/images.js?api_key='+app.api_key+'';	
+	}
+});
+app.collection.Categories = app.collection.EtsySearch.extend({
+	url: function() {
+		return 'http://openapi.etsy.com/v2/taxonomy/categories.js?api_key='+app.api_key;
 	}
 });
 app.view.ProductImages = Backbone.View.extend({
@@ -115,6 +126,35 @@ app.view.Results = Backbone.View.extend({
 		this.trigger('viewProduct', model);
 	}
 });
+app.view.Category = Backbone.View.extend({
+	className: 'category',
+	initialize: function() {
+		_.bindAll(this);
+		this.template = Handlebars.compile($('#category-template').html());
+	},
+	render: function() {
+		this.$el.html(this.template(this.model.attributes));
+		return this;	
+	}
+});
+app.view.Categories = Backbone.View.extend({
+	el: '#categories',
+	initialize: function() {
+		_.bindAll(this);
+		this.collection = new app.collection.Categories();
+		this.collection.on('scoped', this.render);	
+		this.collection.fetch({dataType: "jsonp", success: this.collection.scope});
+		this.categories = {};
+	},
+	render: function() {
+		this.$el.empty();
+    	this.collection.each(this.addOne);
+	},
+	addOne: function(category) {
+		this.categories[category.get('category_id')] = new app.view.Category({model: category});
+    	this.$el.append(this.categories[category.get('category_id')].render().el);
+	}
+});
 app.view.EtsySearch = Backbone.View.extend({
 	el: '#app',
 	events: {
@@ -130,6 +170,7 @@ app.view.EtsySearch = Backbone.View.extend({
 			collection: this.listings
 		});
 		this.results.on('viewProduct', this.viewProduct);
+		this.categories = new app.view.Categories();
 	},
 	render: function() {
 		this.$el.html(this.template());
@@ -138,6 +179,7 @@ app.view.EtsySearch = Backbone.View.extend({
 		e.preventDefault();
 		this.listings.searchTerm = this.$el.find('.search-term').val();
 		this.listings.fetch({dataType: "jsonp", success: this.listings.scope});
+		this.categories.$el.show();
 	},
 	viewProduct: function(model) {
 		this.product = new app.view.Product({model: model});
